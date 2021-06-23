1a7fb4d85b4005834f52935f478146dc4c49cf5f
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-06-23 15:53:38 +0300

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,10 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   host = 'morning-crag-96208.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
@@ -87,7 +89,7 @@ Rails.application.configure do
     user_name: ENV['SMTP_USER_NAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: :plain,
-    domain: 'heroku.com',
+    domain: "gmail.com",
     enable_starttls_auto: true
   }
 end
