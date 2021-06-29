ea4eccbd031413b09385a26f184d5d283ff0005b
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-06-29 09:50:13 +0300

    fix aws config try-1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,13 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET')
+    config.aws_acl    = 'public-read'
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
+      secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+      region:            ENV.fetch('AWS_REGION') # Required
     }
-    config.fog_directory = ENV['S3_BUCKET']
   end
 end
