9aa19560b7dbc0a23cc1a2956566f0f3e0894738
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-06-29 10:42:35 +0300

    fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       :aws_access_key_id =>    ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :region => ENV['AWS_REGION'],
-      :aws_signature_version => 4
+      :aws_signature_version => :v4
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  
 
